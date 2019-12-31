18fc24af6899ffe672355c3141a90bdbb6dd2a0c
Huan LI <zixia@zixia.net>
2019-12-31 22:47:16 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Wechaty Github App
+
+[![Build Status](https://travis-ci.com/wechaty/github-app-wechaty.svg?branch=master)](https://travis-ci.com/wechaty/github-app-wechaty)
+[![Greenkeeper badge](https://badges.greenkeeper.io/wechaty/github-app-wechaty.svg)](https://greenkeeper.io/)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+> A GitHub App built with [Probot](https://github.com/probot/probot)
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run typescript
+npm run build
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how Chatie could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## Resources
+
+- [PROBOT - GitHub Apps to automate and improve your workflow Use pre-built apps to extend GitHub, and easily build and share your own.](https://probot.github.io/)
+
+## History
+
+### master
+
+### v0.0.1 Dec 31, 2019
+
+1. Init from Probot: [Getting Started - Developing an app](https://probot.github.io/docs/development/)
+1. Deployed to Heroku at <https://github-app-wechaty.herokuapp.com>
+1. Published Github App at <https://github.com/apps/wechaty/>
+
+## Author
+
+[Huan](https://github.com/huan) [(李卓桓)](http://linkedin.com/in/zixia) <zixia@zixia.net>
+
+[![Profile of Huan LI (李卓桓) on StackOverflow](https://stackoverflow.com/users/flair/1123955.png)](https://stackoverflow.com/users/1123955/huan)
+
+## Copyright & License
+
+- Code & Docs © 2019-now Huan LI (李卓桓) <zixia@zixia.net>
+- Code released under the Apache-2.0 License
+- Docs released under Creative Commons
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import {
+  OnCallback,
+}              from 'probot/lib/application'
+import Webhooks from '@octokit/webhooks'
+
+import { UrlLinkPayload } from 'wechaty-puppet'
+
+import FileBox from 'file-box'
+
+let n = 0
+
+const openIssue: OnCallback<Webhooks.WebhookPayloadIssues> = async (context) => {
+  const fullName = context.payload.repository.full_name
+  const issueNumber = context.payload.issue.number
+  const issueTitle = context.payload.issue.title
+  const issueBody = context.payload.issue.body
+  const htmlUrl = context.payload.issue.html_url
+  const avatarUrl = context.payload.repository.owner.avatar_url
+
+  const title = [
+    fullName,
+    `#${issueNumber}`,
+    issueTitle.slice(0, Math.max(issueTitle.length, 30)),
+  ].join(' ')
+  const url = htmlUrl
+  const description = issueBody.slice(0, Math.max(issueBody.length, 70))
+  const thumbnailUrl = avatarUrl
+
+  await callWebhook({
+    description,
+    thumbnailUrl,
+    title,
+    url,
+  })
+}
+
+const commentIssue: OnCallback<Webhooks.WebhookPayloadIssueComment> = async (context) => {
+  // const issue = context .issue()
+  // console.info(context.payload.repository)
+  const fullName = context.payload.repository.full_name
+  const issueNumber = context.payload.issue.number
+  const issueTitle = context.payload.issue.title
+  const commentBody = context.payload.comment.body
+  const htmlUrl = context.payload.comment.html_url
+  const avatarUrl = context.payload.comment.user.avatar_url
+
+  const title = [
+    fullName,
+    `#${issueNumber}`,
+    issueTitle.slice(0, Math.max(issueTitle.length, 30)),
+  ].join(' ')
+  const url = htmlUrl
+  const description = commentBody.slice(0, Math.max(commentBody.length, 70))
+  const thumbnailUrl = avatarUrl
+
+  await callWebhook({
+    description,
+    thumbnailUrl,
+    title,
+    url,
+  })
+
+  // const issueComment = context.issue({ body: `Thanks for comment this issue! ${n++}` })
+  // await context.github.issues.createComment(issueComment)
+  // console.info(context)
+}
+
+async function callWebhook (
+  payload: UrlLinkPayload,
+) {
+  console.info('callWebhook:', JSON.stringify(payload))
+
+  const url = [
+    'https://friday.bot5.club/webhook/',
+    [
+      `url=${encodeURIComponent(payload.url)}`,
+      `description=${encodeURIComponent(payload.description || '')}`,
+      `thumbnailUrl=${encodeURIComponent(payload.thumbnailUrl || '')}`,
+      `title=${encodeURIComponent(payload.title)}`,
+    ].join('&'),
+  ].join('?')
+
+  const result = (await FileBox.fromUrl(url).toBuffer()).toString()
+  console.info('result:', result)
+}
+
+export = (app: Application) => {
+  app.on('issue_comment.created', commentIssue)
+  app.on('issues.opened', openIssue)
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
